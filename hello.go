f042b477f9e8c314b925e44ecbbaa215b4b4c635
Derek Taylor <Derek.Taylor@mgramx.com>
2017-06-13 18:35:08 -0700

    Use stringutil package

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/datadam/stringutil"
+)
+   
 
+// Comment
 func main() {
     fmt.Printf("Hello, World\n")
+    fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
